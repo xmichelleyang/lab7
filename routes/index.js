cab81a628b22b9cefd3f4bc69c31924b1f89f998
Michelle Yang <zomgitsmichelle@gmail.com>
2019-02-21 01:33:53 -0800

    Most things working.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ var projects = require('../projects.json');
 
 exports.view = function(request, response){
   	response.render('index', projects);
+  	projects["viewAlt"] = false;
+  	console.log("normal view", projects["viewAlt"])
+  	viewAlt = false;
+};
+
+exports.viewAlt = function(request, response){
+  	response.render('index', projects);
+  	projects["viewAlt"] = true;
+  	console.log("not normal", projects["viewAlt"]);
+  	console.log(projects.viewAlt);
+  	viewAlt = true;
 };
